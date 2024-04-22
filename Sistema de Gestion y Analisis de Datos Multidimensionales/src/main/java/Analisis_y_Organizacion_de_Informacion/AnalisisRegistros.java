cc4d75cf9bf991e441572751345e1d4daea71b32
rodrigoyr <ryeperub@myuax.com>
2024-04-22 12:00:33 +0200

    commit

--- Sistema de Gestion y Analisis de Datos Multidimensionales/src/main/java/Analisis_y_Organizacion_de_Informacion/AnalisisRegistros.java
+++ Sistema de Gestion y Analisis de Datos Multidimensionales/src/main/java/Analisis_y_Organizacion_de_Informacion/AnalisisRegistros.java
@@ -1,4 +1,25 @@
 package Analisis_y_Organizacion_de_Informacion;
 
+import Gestion_de_Datos_Dinamicos.Pareja;
+import Gestion_de_Datos_Dinamicos.ListaDatos;
+
 public class AnalisisRegistros {
+    private ListaDatos listaDatos;
+
+    // Constructor
+    public AnalisisRegistros(ListaDatos listaDatos) {
+        this.listaDatos = listaDatos;
+    }
+
+    // Método para ordenar los registros
+    // Este método es solo un placeholder, necesitarás implementar la lógica de ordenación
+    public void ordenarRegistros() {
+        System.out.println("Este método ordenará los registros");
+    }
+
+    // Método para filtrar los registros
+    // Este método es solo un placeholder, necesitarás implementar la lógica de filtrado
+    public void filtrarRegistros() {
+        System.out.println("Este método filtrará los registros");
+    }
 }
