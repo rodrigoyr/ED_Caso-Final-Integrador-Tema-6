31ced0da12dac2fc843acd9e9574bacb36bbd877
rodrigoyr <ryeperub@myuax.com>
2024-04-22 12:03:58 +0200

    commit

--- Sistema de Gestion y Analisis de Datos Multidimensionales/src/main/java/Indexacion_y_Visualizacion_de_Archivos/IndexacionRecursiva.java
+++ Sistema de Gestion y Analisis de Datos Multidimensionales/src/main/java/Indexacion_y_Visualizacion_de_Archivos/IndexacionRecursiva.java
@@ -1,4 +1,32 @@
 package Indexacion_y_Visualizacion_de_Archivos;
 
+import java.io.File;
+import java.util.ArrayList;
+import java.util.List;
+
 public class IndexacionRecursiva {
+    private List<File> archivosIndexados;
+
+    // Constructor
+    public IndexacionRecursiva() {
+        this.archivosIndexados = new ArrayList<>();
+    }
+
+    // Método para indexar archivos de manera recursiva
+    public void indexarArchivos(File directorio) {
+        // Si el archivo es un directorio, indexamos recursivamente
+        if (directorio.isDirectory()) {
+            for (File archivo : directorio.listFiles()) {
+                indexarArchivos(archivo);
+            }
+        } else {
+            // Si el archivo no es un directorio, lo añadimos a la lista de archivos indexados
+            archivosIndexados.add(directorio);
+        }
+    }
+
+    // Método para obtener la lista de archivos indexados
+    public List<File> obtenerArchivosIndexados() {
+        return this.archivosIndexados;
+    }
 }
