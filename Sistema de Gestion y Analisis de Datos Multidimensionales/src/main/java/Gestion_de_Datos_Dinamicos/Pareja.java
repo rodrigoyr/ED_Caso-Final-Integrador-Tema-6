90d963bc6646391f98c139a20ec8cd887d7517ff
rodrigoyr <ryeperub@myuax.com>
2024-04-22 11:58:06 +0200

    commit

--- Sistema de Gestion y Analisis de Datos Multidimensionales/src/main/java/Gestion_de_Datos_Dinamicos/Pareja.java
+++ Sistema de Gestion y Analisis de Datos Multidimensionales/src/main/java/Gestion_de_Datos_Dinamicos/Pareja.java
@@ -1,4 +1,30 @@
 package Gestion_de_Datos_Dinamicos;
 
 public class Pareja {
+    private int elemento1;
+    private int elemento2;
+
+    // Constructor
+    public Pareja(int elemento1, int elemento2) {
+        this.elemento1 = elemento1;
+        this.elemento2 = elemento2;
+    }
+
+    // Getters
+    public int getElemento1() {
+        return this.elemento1;
+    }
+
+    public int getElemento2() {
+        return this.elemento2;
+    }
+
+    // Setters
+    public void setElemento1(int elemento1) {
+        this.elemento1 = elemento1;
+    }
+
+    public void setElemento2(int elemento2) {
+        this.elemento2 = elemento2;
+    }
 }
