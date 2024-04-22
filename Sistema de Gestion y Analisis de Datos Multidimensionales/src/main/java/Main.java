cf247f32c2960efe44721da7ceaf4a6cc5e9e70d
rodrigoyr <ryeperub@myuax.com>
2024-04-22 12:06:25 +0200

    commit

--- Sistema de Gestion y Analisis de Datos Multidimensionales/src/main/java/Main.java
+++ Sistema de Gestion y Analisis de Datos Multidimensionales/src/main/java/Main.java
@@ -1,2 +1,25 @@
+import Indexacion_y_Visualizacion_de_Archivos.IndexacionRecursiva;
+import Indexacion_y_Visualizacion_de_Archivos.OrdenacionListado;
+
+import java.io.File;
+
 public class Main {
+    public static void main(String[] args) {
+        // Crear una instancia de IndexacionRecursiva
+        IndexacionRecursiva indexacionRecursiva = new IndexacionRecursiva();
+
+        // Indexar los archivos en un directorio específico
+        indexacionRecursiva.indexarArchivos(new File("ruta/a/tu/directorio"));
+
+        // Crear una instancia de OrdenacionListado con los archivos indexados
+        OrdenacionListado ordenacionListado = new OrdenacionListado(indexacionRecursiva.obtenerArchivosIndexados());
+
+        // Ordenar los archivos por nombre
+        ordenacionListado.ordenarPorNombre();
+
+        // Imprimir los archivos ordenados
+        for (File archivo : ordenacionListado.obtenerArchivosIndexados()) {
+            System.out.println(archivo.getName());
+        }
+    }
 }
