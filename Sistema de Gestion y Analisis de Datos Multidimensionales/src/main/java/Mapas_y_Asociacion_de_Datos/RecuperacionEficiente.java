1c41708f62ac2e37eefb97a53f0f19272d9dab18
rodrigoyr <ryeperub@myuax.com>
2024-04-22 12:03:19 +0200

    commit

--- Sistema de Gestion y Analisis de Datos Multidimensionales/src/main/java/Mapas_y_Asociacion_de_Datos/RecuperacionEficiente.java
+++ Sistema de Gestion y Analisis de Datos Multidimensionales/src/main/java/Mapas_y_Asociacion_de_Datos/RecuperacionEficiente.java
@@ -1,4 +1,18 @@
 package Mapas_y_Asociacion_de_Datos;
 
+import Gestion_de_Datos_Dinamicos.Pareja;
+import java.util.Map;
+
 public class RecuperacionEficiente {
+    private Map<Pareja, Pareja> relaciones;
+
+    // Constructor
+    public RecuperacionEficiente(Map<Pareja, Pareja> relaciones) {
+        this.relaciones = relaciones;
+    }
+
+    // Método para recuperar una relación de manera eficiente
+    public Pareja recuperarRelacion(Pareja pareja) {
+        return this.relaciones.get(pareja);
+    }
 }
