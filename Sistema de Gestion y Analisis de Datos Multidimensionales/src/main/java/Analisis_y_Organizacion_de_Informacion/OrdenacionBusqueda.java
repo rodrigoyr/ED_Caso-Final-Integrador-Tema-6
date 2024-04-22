60277d8beaec8d688c80e149fed422bd41b8c678
rodrigoyr <ryeperub@myuax.com>
2024-04-22 12:01:35 +0200

    commit

--- Sistema de Gestion y Analisis de Datos Multidimensionales/src/main/java/Analisis_y_Organizacion_de_Informacion/OrdenacionBusqueda.java
+++ Sistema de Gestion y Analisis de Datos Multidimensionales/src/main/java/Analisis_y_Organizacion_de_Informacion/OrdenacionBusqueda.java
@@ -1,4 +1,46 @@
 package Analisis_y_Organizacion_de_Informacion;
 
+import Gestion_de_Datos_Dinamicos.Pareja;
+import Gestion_de_Datos_Dinamicos.ListaDatos;
+
+import java.util.Collections;
+import java.util.Comparator;
+
 public class OrdenacionBusqueda {
+    private ListaDatos listaDatos;
+
+    // Constructor
+    public OrdenacionBusqueda(ListaDatos listaDatos) {
+        this.listaDatos = listaDatos;
+    }
+
+    // Método para ordenar los registros por el primer elemento de la pareja
+    public void ordenarPorElemento1() {
+        Collections.sort(listaDatos.obtenerLista(), Comparator.comparing(Pareja::getElemento1));
+    }
+
+    // Método para ordenar los registros por el segundo elemento de la pareja
+    public void ordenarPorElemento2() {
+        Collections.sort(listaDatos.obtenerLista(), Comparator.comparing(Pareja::getElemento2));
+    }
+
+    // Método para buscar una pareja por el primer elemento
+    public Pareja buscarPorElemento1(int elemento1) {
+        for (Pareja pareja : listaDatos.obtenerLista()) {
+            if (pareja.getElemento1() == elemento1) {
+                return pareja;
+            }
+        }
+        return null;
+    }
+
+    // Método para buscar una pareja por el segundo elemento
+    public Pareja buscarPorElemento2(int elemento2) {
+        for (Pareja pareja : listaDatos.obtenerLista()) {
+            if (pareja.getElemento2() == elemento2) {
+                return pareja;
+            }
+        }
+        return null;
+    }
 }
