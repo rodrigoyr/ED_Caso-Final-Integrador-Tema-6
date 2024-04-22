ed0dd5c269c0869047c50dac9f6374c47c75d40f
rodrigoyr <ryeperub@myuax.com>
2024-04-22 11:58:52 +0200

    commit

--- Sistema de Gestion y Analisis de Datos Multidimensionales/src/main/java/Gestion_de_Datos_Dinamicos/ListaDatos.java
+++ Sistema de Gestion y Analisis de Datos Multidimensionales/src/main/java/Gestion_de_Datos_Dinamicos/ListaDatos.java
@@ -1,4 +1,32 @@
 package Gestion_de_Datos_Dinamicos;
 
+import java.util.ArrayList;
+
 public class ListaDatos {
+    private ArrayList<Pareja> lista;
+
+    // Constructor
+    public ListaDatos() {
+        this.lista = new ArrayList<>();
+    }
+
+    // Método para agregar una pareja a la lista
+    public void agregarPareja(Pareja pareja) {
+        this.lista.add(pareja);
+    }
+
+    // Método para eliminar una pareja de la lista
+    public void eliminarPareja(Pareja pareja) {
+        this.lista.remove(pareja);
+    }
+
+    // Método para obtener una pareja de la lista
+    public Pareja obtenerPareja(int index) {
+        return this.lista.get(index);
+    }
+
+    // Método para obtener la lista completa
+    public ArrayList<Pareja> obtenerLista() {
+        return this.lista;
+    }
 }
